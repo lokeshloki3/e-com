7c6c571b49c0a35e96f5d5fa8f85957a4b69fc0c
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-02 00:45:37 +0530

    db timeout

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectWithDb = () => {
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL,{
+        serverSelectionTimeoutMS: 30000,
+    })
     .then(console.log("DB connected successfully"))
     .catch((error) => {
         console.log("DB facing connection issues");
